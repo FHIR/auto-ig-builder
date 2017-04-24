6b3885a4b0bf9eeba9f53bc82e700968b63df370
J M <jmandel@test-jenkins>
2017-04-24 18:32:54 -0400

    Add cloud function to initate k8s job

---
+++ ig-commit-trigger/index.js
@@ -0,0 +1,45 @@
+var secret = require('./secret.json');
+var job = require('./job.json');
+var decode = require('base-64').decode;
+
+var config = {
+  url: 'https://35.190.130.127',
+  ca: decode(secret.data['ca.crt']),
+  auth: {
+    bearer: decode(secret.data.token)
+  }
+};
+
+var Api = require('kubernetes-client');
+const core = new Api.Core(config);
+const batch = new Api.Batch(config);
+
+function print(err, result) {
+  console.log(JSON.stringify(err || result, null, 2));
+}
+
+exports["ig-commit-trigger"] = function(req, res) {
+
+    console.log("BODY 2", req.body);
+    var target = req.body.repository.full_name.split('/');
+    var org = target[0];
+    var repo = target[1];
+
+    console.log("JOB", job);
+
+    var env = job.spec.template.spec.containers[0].env;
+    var envorg  = env[0];
+    var envrepo = env[1];
+
+    envorg.value = org;
+    envrepo.value = repo;
+
+    batch.ns('fhir').jobs.post({body: job}, function(err, submitted){
+      res.status(200).json({
+        'org': org,
+        'repo': repo,
+        'submitted': submitted
+      });
+    })
+
+};
---
+++ make-gce-instance.sh
@@ -0,0 +1,21 @@
+gcloud container clusters create fhir-k8s \
+    --num-nodes 1 \
+    --disk-size 100 \
+    --machine-type n1-standard-4 \
+    --scopes cloud-platform
+
+gcloud compute --project "fhir-org-starter-project" \
+    instances create "fhir-build" \
+    --zone "us-east1-d" 
+    --machine-type "custom-2-8192" \
+    --subnet "default" --address 35.185.17.144 \
+    --metadata "ssh-keys=jmandel:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC02Vm42jsJhGRmHp85AJGPrYYZYCY5lA9AOKtz8Dj0OL7+lVHiGYQp8atFVrGc384wOQraY0+I57mXzKnGksgSmZ11LgZ/JQousB3qTMNUUVPIy6lgnASx51YxQCZzS3Yg6ocAemcUSiMOOZxpTLXIe1e9USPKRcuNDDGSQWkTP/rp78/PGFMMYHNjex21ioEFzEKbP96K0rImU9pkQ1ndEYj4+NDuC1OjmPOIUvwJwpGfUzceYiHtwfe6eyBXKKEp+475bW+NDobns2idWRJCkMY5i3djhVNGCzpRHzuO0BzP6wWRyKZ/M0/KRHGhgbEnbMOvu8XkjTtJyEE+p0QL jmandel@morel" \
+    --maintenance-policy "MIGRATE" \
+    --service-account "fhir-build-gce@fhir-org-starter-project.iam.gserviceaccount.com"\
+    --scopes "https://www.googleapis.com/auth/cloud-platform" \
+    --tags "http-server","https-server" \
+    --image "ubuntu-1604-xenial-v20170330" \
+    --image-project "ubuntu-os-cloud" \
+    --boot-disk-size "100" \
+    --boot-disk-type "pd-standard" \
+    --boot-disk-device-name "fhir-build-boot-disk"
