69fcf7f2a9759dedd85cffde8b57f6f69e71e2d1
Josh Mandel <jmandel@med.harvard.edu>
2017-04-26 01:10:59 +0000

    Add svn-sync image

--- k8s/create.sh
+++ k8s/create.sh
@@ -2,5 +2,13 @@ docker tag static  gcr.io/fhir-org-starter-project/ci-build
 gcloud docker -- push gcr.io/fhir-org-starter-project/ci-build
 
 gcloud compute disks create fhir-ci-build-disk --size 20GB
+gcloud compute disks create fhir-svn-sync-disk --size 20GB
 
-kubectl run --namespace fhir  ci-build  --image=gcr.io/fhir-org-starter-project/ci-build
+# kubectl run --namespace fhir  ci-build  --image=gcr.io/fhir-org-starter-project/ci-build
+
+kubectl  --namespace fhir create secret generic deploy.build.fhir.org \
+  --from-file deploy.build.fhir.org \
+  --from-file deploy.build.fhir.org.pub
+
+kubectl apply -f ci-build.deployment 
+kubectl apply -f svn-sync.deployment 
