28a659383b5eb2deb522569081b64bb27f20ee7c
Josh Mandel <jmandel@med.harvard.edu>
2017-04-26 15:36:14 +0000

    bootstrap git

--- k8s/create.sh
+++ k8s/create.sh
@@ -6,9 +6,9 @@ gcloud compute disks create fhir-svn-sync-disk --size 20GB
 
 # kubectl run --namespace fhir  ci-build  --image=gcr.io/fhir-org-starter-project/ci-build
 
-kubectl  --namespace fhir create secret generic deploy.build.fhir.org \
-  --from-file deploy.build.fhir.org \
-  --from-file deploy.build.fhir.org.pub
+kubectl  --namespace fhir create secret generic github-fhir-svn  \
+  --from-file ~/Private/deploy_fhir_svn \
+  --from-file ~/Private/deploy_fhir_svn.pub
 
 kubectl apply -f ci-build.deployment 
 kubectl apply -f svn-sync.deployment 
