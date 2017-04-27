18fae4aaa2efe8a13c4edb5e7e04e2c44bb8b5b0
Josh Mandel <jmandel@med.harvard.edu>
2017-04-27 00:19:48 +0000

    Fix paths

--- ig-build/builder/builder.py
+++ ig-build/builder/builder.py
@@ -50,18 +50,18 @@ def build(config):
     details['buildlog'] = 'build.log'
     message += [" | [debug](%(root)s/%(org)s/%(repo)s)"]
     shutil.copy(logfile, clone_dir)
-    do(['upload', details['org'], details['repo'], clone_dir)
+    do(['publish', details['org'], details['repo']], clone_dir)
   else:
     print "Build succeeded"
     details['emoji'] = 'thumbsup'
     details['buildlog'] = 'build.log'
     message += [" | [published](%(root)s/%(org)s/%(repo)s/index.html)"]
     shutil.copy(logfile, build_dir)
-    do(['upload', details['org'], details['repo'], build_dir)
+    do(['publish', details['org'], details['repo']], build_dir)
 
   shutil.rmtree(temp_dir)
   send_zulip('committers', 'ig-build', "".join(message)%details)
-  sys.exit(0 if built else 1)
+  # sys.exit(0 if built else 1)
 
 if __name__ == '__main__':
   build({
