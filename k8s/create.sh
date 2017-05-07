a958d24f44bb96dafc60c83bba2b72818b0c3a07
J M <jmandel@test-jenkins>
2017-05-07 13:16:35 +0200

    Add hapi ref server

--- k8s/create.sh
+++ k8s/create.sh
@@ -13,7 +13,10 @@ kubectl  --namespace fhir create secret generic github-fhir-svn  \
 
 kubectl  --namespace fhir create secret generic ci-build-keys  --from-file=id=/home/jmandel/Private/deploy.build.fhir.org --from-file=id.pub=/home/jmandel/Private/deploy.build.fhir.org.pub
 kubectl  --namespace fhir create secret generic gforge-secrets --from-literal=email=$ZULIP_EMAIL --from-literal=api_key=$ZULIP_API_KEY --from-literal=gforge_password=$GFORGE_PASSWORD
+ kubectl  --namespace fhir create secret generic fhir-org-ssl-keys --from-file=tls.crt=/home/jmandel/Private/certs/fhir.org.cert.pem  --from-file=ca.crt=/home/jmandel/Private/certs/fhir.org.intermediate.cert.pem  --from-file=tls.key=/home/jmandel/Private/certs/fhir.org.key.pem --from-file=tls.chained.crt=/home/jmandel/Private/certs/fhir.org.chained.pem
 
+kubectl apply -f ci-build.configmap.yaml
+kubectl apply -f hapi.deployment.yaml  -f hapi.service.yaml
 kubectl apply -f ci-build.deployment.yaml
 kubectl apply -f svn-sync.deployment.yaml
 kubectl apply -f gforge-to-zulip.deployment.yaml
