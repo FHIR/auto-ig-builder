9aeadc7f24430eb53e56b49ddbe31b4490eeec0b
Josh Mandel <jmandel@alum.mit.edu>
2024-05-16 22:58:23 -0500

    Update flake WIP

--- images/ig-publisher/flake.nix
+++ images/ig-publisher/flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fhirPublisherJarFile = {
+      url = "https://github.com/HL7/fhir-ig-publisher/releases/download/1.6.7/publisher.jar";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, fhirPublisherJarFile }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       tarballPath = pkgs.dockerTools.pullImage {
@@ -18,33 +22,39 @@
     in
     {
       packages.x86_64-linux.default = pkgs.dockerTools.buildImage {
-        name = "your-image-name";
+        name = "ghcr.io/fhir/ig-publisher-localdev-nix";
         tag = "latest";
         fromImage = tarballPath;
         copyToRoot = pkgs.buildEnv {
           name = "image-root";
           paths = with pkgs; [
             shadow
+            curl
+            wget
+            jq
             bash
-            jre_minimal
-            ruby
-            rubyPackages.jekyll
-            #rubyPackages.jekyll-asciidoc
+            jdk
+            bashInteractive
+            readline
+            ncurses
+            jekyll
             nodejs
             nodePackages.npm
             git
             graphviz
             openssl
             (pkgs.runCommand "copy-files" {} ''
-              mkdir -p $out/app/builder
               mkdir -p $out/usr/local/bin
               cp ${./localdev-files/docker-entrypoint.sh} $out/usr/local/bin/docker-entrypoint.sh
+              mkdir -p $out/app/lib
+              cp ${fhirPublisherJarFile} $out/app/lib/publisher.jar
             '')
           ];
         };
         config = {
+          Entrypoint = [ "/usr/local/bin/docker-entrypoint.sh" ];
           Cmd = [ "bash" ];
-          WorkingDir = "/app";
+          WorkingDir = "/home/publisher/ig";
           Env = [
             "PATH=/usr/local/bin:/usr/bin:/bin:/home/publisher/bin:/home/publisher/bin/ig-publisher-scripts:/home/publisher/.node/bin"
           ];
@@ -57,14 +67,14 @@
         runAsRoot = ''
           useradd -d /home/publisher -m publisher
           chown -R publisher:publisher /home/publisher
-        '';
-
-        run = ''
+          su - publisher -c '
             mkdir /home/publisher/ig
             mkdir /home/publisher/.node
             echo "prefix = /home/publisher/.node" > /home/publisher/.npmrc
             mkdir /home/publisher/bin
             git config --global pull.ff only
+          '
+
         '';
 
       };
