4c24d3dc752f32e4d6d4f850de9f5a2b14955285
Josh Mandel <jmandel@alum.mit.edu>
2024-05-30 11:52:32 -0500

    Build separate for gh

--- images/ig-publisher/localdev-files/docker-entrypoint.sh
+++ images/ig-publisher/localdev-files/docker-entrypoint.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/HL7/ig-publisher-scripts /home/publisher/bin/ig-publisher-scripts
-npm install -g fsh-sushi
+# Change uid and gid of node user so it matches ownership of current dir
+if [ "$MAP_NODE_UID" != "no" ]; then
+    if [ ! -d "$MAP_NODE_UID" ]; then
+        MAP_NODE_UID=$PWD
+    fi
 
-exec "$@"
+    uid=$(stat -c '%u' "$MAP_NODE_UID")
+    gid=$(stat -c '%g' "$MAP_NODE_UID")
+    echo "publisher ---> UID = $uid / GID = $gid"
+
+    export USER=publisher
+
+    usermod -u $uid publisher 2> /dev/null && {
+      groupmod -g $gid publisher 2> /dev/null || usermod -a -G $gid publisher
+    }
+fi
+
+gosu publisher git clone https://github.com/HL7/ig-publisher-scripts /home/publisher/bin/ig-publisher-scripts
+gosu publisher npm install -g fsh-sushi
+exec gosu publisher "$@"
